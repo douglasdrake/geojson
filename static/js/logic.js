9678a599949a597f3eaf60dae7d7478cbd1edff7
Douglas Drake <dougdrake42@gmail.com>
2019-06-29 13:44:58 -0500

    Added id info

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,3 +1,7 @@
+// Douglas Drake - Cohort 3 - Homework 17
+
+const API_KEY = "YOUR API KEY HERE";
+
 // Store our API endpoint inside QueryUrls
 var earthquakeQueryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
@@ -126,7 +130,7 @@ function createMap(earthquakes, plates) {
     center: [
       37.09, -95.71
     ],
-    zoom: 5,
+    zoom: 3,
     layers: [satelliteMap, earthquakes, plates]
   });
 
